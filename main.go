53f6712b9e7d8c3b762ebbad856c32b712e1b709
Leandro Lima <llimas@outlook.com>
2021-11-28 15:59:08 -0300

    Adding comments

--- main.go
+++ main.go
@@ -1,9 +1,11 @@
 package valoop
 
+// IsSameValue check if interface a has the same value of interface b
 func IsSameValue(a interface{}, b interface{}) bool {
 	return a == b
 }
 
+// IntSliceContains check if int slice s contains int e
 func IntSliceContains(s []int, e int) bool {
 	for _, a := range s {
 		if a == e {
